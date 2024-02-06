dd48556df8f0dc58f8e930077837b819dfefd71e
NILESH GULE <vn_nilesh@yahoo.com>
2024-02-06 20:31:05 +0800

    refactored test code

--- java/src/test/java/com/example/restservice/greeting/GreetingControllerTests.java
+++ java/src/test/java/com/example/restservice/greeting/GreetingControllerTests.java
@@ -38,7 +38,7 @@ public class GreetingControllerTests {
 	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
 
 		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-				.andExpect(jsonPath("$.content").value("Hello, World!"));
+				.andExpect(jsonPath("$.content").value("Hello, Docker!"));
 	}
 
 	@Test
